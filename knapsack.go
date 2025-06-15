53eec56b9dce3c64845d6f21b89119be56819d67
Sam De Boni <sdeboni@gmail.com>
2025-06-15 06:00:56 -0400

    dynamic programming solution
    dynamic programming:   27478 ns/op
    brute force:         1846260 ns/op

--- knapsack.go
+++ knapsack.go
@@ -1,6 +1,3 @@
-// This is a brute force solution: all possible combinations are evaluated
-// The next iteration will focus on optimizing performance
-
 package knapsack
 
 type Item struct {
@@ -11,60 +8,30 @@ type Item struct {
 // and returns the maximum value that can be carried by the knapsack
 // given that the knapsack can only carry a maximum weight given by maximumWeight
 func Knapsack(maximumWeight int, items []Item) int {
-  var best int
 
-  for i := 0; i < len(items); i++ {
-    for _, combination := range choose(i, items) {
-      weight, value := total(combination)
-      if weight <= maximumWeight && value > best {
-        best = value
-      }
-    } 
-  }
-  return best
-}
-
-func total(items []Item) (weight int, value int) {
-  for _, item := range items {
-    weight += item.Weight
-    value += item.Value
-  }
-  return
-}
-
-func choose[T any](n int, items []T) [][]T {
-  var result [][]T
-
-  N := len(items)
-   
-  a := make([]int, n)
-  for i := 0; i < n; i++ {
-    a[i] = i
-  }
-  result = add(result, a, items)
-  
-  i := n-1
-  for i >= 0 {
-    for a[i] < i + N - n {
-      a[i] = a[i]+1
-      i++
-      for i < n {
-        a[i] = a[i-1] + 1
-        i++
-      }
-      result = add(result, a, items)
-      i--
-    }
-    i--
+  //dynamic programming solution
+  //source: https://en.wikipedia.org/wiki/Knapsack_problem
+  var m = make([][]int, len(items)+1)
+  for i := 0; i < len(m); i++ {
+    m[i] = make([]int, maximumWeight+1)
   } 
-  return result
-}
-func add[T any](a [][]T, b []int, v []T) [][]T {
-  c := make([]T, len(b))
-  for i := 0; i < len(b); i++ {
-    c[i] = v[b[i]]
+
+  var max = func(a, b int) int {
+    if a >= b {
+      return a
+    }
+    return b
   }
-  return append(a, c)
+
+  for i := 1; i < len(m); i++ {
+    for j := 1; j <= maximumWeight; j++ {
+      if items[i-1].Weight > j {
+        m[i][j] = m[i-1][j]
+      } else {
+        m[i][j] = max(m[i-1][j], m[i-1][j-items[i-1].Weight]+ items[i-1].Value)
+      }
+    }
+  }
+
+  return m[len(items)][maximumWeight]
 }
-
-
